a2bef535817bea0e8e37b01153015222ea8589af
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-19 02:19:31 -0500

    run update

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,19 @@ var jshint = require('gulp-jshint');
 
 var lint = ['index.js', 'utils.js'];
 
-gulp.task('coverage', function () {
+gulp.task('coverage', function() {
   return gulp.src(lint)
     .pipe(istanbul())
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('test', ['coverage'], function () {
+gulp.task('test', ['coverage'], function() {
   return gulp.src('test.js')
     .pipe(mocha({reporter: 'spec'}))
     .pipe(istanbul.writeReports());
 });
 
-gulp.task('lint', function () {
+gulp.task('lint', function() {
   return gulp.src(lint)
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
